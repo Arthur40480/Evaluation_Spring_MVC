a1de8260033a41e1c9b04bb215850d407359332f
Arthur Gibert <arthurgibert@free.fr>
2024-04-29 11:05:17 +0200

    Ajout des repository OK

---
+++ src/main/java/fr/ldnr/dao/CityRepository.java
@@ -0,0 +1,9 @@
+package fr.ldnr.dao;
+
+import fr.ldnr.entities.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Long> {
+}
---
+++ src/main/java/fr/ldnr/dao/SessionRepository.java
@@ -0,0 +1,7 @@
+package fr.ldnr.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SessionRepository {
+}
