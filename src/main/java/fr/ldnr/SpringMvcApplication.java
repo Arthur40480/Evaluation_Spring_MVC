3e672b361a5977d3d960aba99fea3e72663ce86b
Arthur Gibert <arthurgibert@free.fr>
2024-04-29 10:00:00 +0200

    Initialisation du projet

---
+++ src/main/java/fr/ldnr/SpringMvcApplication.java
@@ -0,0 +1,13 @@
+package fr.ldnr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcApplication.class, args);
+	}
+
+}
